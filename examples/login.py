f5fb9b85d3f2fc303dd721471b5cc3551a96a359
dp118m <dp@altruix.co>
2018-04-18 19:49:50 +0300

    Trying to log in.

--- examples/login.py
+++ examples/login.py
@@ -1,7 +1,24 @@
+"""
+An example of login functionality.
+"""
+from __future__ import absolute_import, division, print_function, with_statement
+from __future__ import unicode_literals
 
+from deepstreampy import connect
+from deepstreampy import rpc
+
+from tornado import gen
+from tornado import ioloop
+
+import signal
+
+@gen.coroutine
 def run():
-    print("Hi there")
+    print("Connecting")
+    provider = yield connect("ws://localhost:6020/deepstream")
 
 if __name__ == "__main__":
     print("Before calling run")
     run()
+    signal.signal(signal.SIGINT, lambda _, __: ioloop.IOLoop.current().stop())
+    ioloop.IOLoop.current().start()
