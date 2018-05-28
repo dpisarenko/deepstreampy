b6aa45b333776346a67f859e279f6b1b15cb3a83
dp118m <dp@altruix.co>
2018-05-28 15:22:52 +0300

    I-2: First version of login seems to work (but not very good).

---
+++ examples/feature-06-logging-in.py
@@ -0,0 +1,32 @@
+"""
+An example showing how to check who is logged into the deepstream.io server and
+listen for changes.
+"""
+from __future__ import absolute_import, division, print_function, with_statement
+from __future__ import unicode_literals
+
+from deepstreampy import connect
+from deepstreampy import presence
+
+from tornado import gen
+from tornado import ioloop
+
+import signal
+
+
+def listener(name, logged_in):
+    if logged_in:
+        print("{} logged in".format(name))
+    else:
+        print("{} logged out".format(name))
+
+@gen.coroutine
+def run():
+    client = yield connect("ws://localhost:6020/deepstream")
+    result = yield client.login({"username": "userA", "password": "password"})
+    print("result: " + str(result['success']))
+
+if __name__ == "__main__":
+    run()
+    signal.signal(signal.SIGINT, lambda _, __: ioloop.IOLoop.current().stop())
+    ioloop.IOLoop.current().start()
